e598d917b0dba8616d0bd833d0dc7c1dfdeed819
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-05-18 08:46:33 +0300

    feat: add Auditable class

--- src/main/java/spring/practice/elmenus_lite/ElmenusLiteApplication.java
+++ src/main/java/spring/practice/elmenus_lite/ElmenusLiteApplication.java
@@ -3,8 +3,6 @@ package spring.practice.elmenus_lite;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import spring.practice.elmenus_lite.repository.CartRepository;
 
 @SpringBootApplication
 public class ElmenusLiteApplication {
