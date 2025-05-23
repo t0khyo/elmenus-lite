dff97900302014e07c9b311ff502fb774744a758
mahmoud Osama <mahmoudosama@Laptop>
2025-05-24 01:57:11 +0300

    feat(mapper): update DTO mapper for improved mapping logic

--- src/main/java/spring/practice/elmenus_lite/mapper/CartMapper.java
+++ src/main/java/spring/practice/elmenus_lite/mapper/CartMapper.java
@@ -11,9 +11,16 @@ import spring.practice.elmenus_lite.model.MenuItem;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
+    @Mapping(source = "id", target = "cartId")
     CartResponse toCartResponse(Cart cart);
+
+    @Mapping(source = "id",target = "cartItemId")
+    @Mapping(target = "totalPrice", expression = "java(BigDecimal.valueOf(cartItem.getQuantity()).multiply(cartItem.getMenuItem().getPrice()))")
     CartItemResponse toCartItemResponse(CartItem cartItem);
 
-    @Mapping(source = "menu.id", target = "menuId")
+    @Mapping(source = "menu.id" , target = "menuId")
+    @Mapping(source = "id", target = "menuItemId")
     MenuItemResponse toMenuItemResponse(MenuItem menuItem);
+
+
 }
