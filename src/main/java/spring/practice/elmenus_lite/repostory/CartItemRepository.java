bd26fb09dcca4fc4ac43ec40f6ce78986e44a213
mahmoud Osama <mahmoudosama@Laptop>
2025-06-01 16:46:02 +0300

    update(Repository): add some method for OrderStatusRepository , CartItemRepository

--- src/main/java/spring/practice/elmenus_lite/repostory/CartItemRepository.java
+++ src/main/java/spring/practice/elmenus_lite/repostory/CartItemRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import spring.practice.elmenus_lite.model.CartItem;
 
+import java.util.List;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
     boolean existsByIdAndCartId(Integer cartItemId, Integer cartId);
+
+    List<CartItem> findByCartId(Integer cartId);
+
+    void deleteByCartId(Integer cartId);
 }
