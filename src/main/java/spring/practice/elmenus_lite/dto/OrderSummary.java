8c0ec053eafc65883a8ab0f7e297d6f8c9cb8ce0
mahmoud Osama <mahmoudosama@Laptop>
2025-06-02 00:39:54 +0300

    Change Field in OrderSummary & PaymentResult DTO

--- src/main/java/spring/practice/elmenus_lite/dto/OrderSummary.java
+++ src/main/java/spring/practice/elmenus_lite/dto/OrderSummary.java
@@ -2,18 +2,17 @@ package spring.practice.elmenus_lite.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
-import spring.practice.elmenus_lite.model.enums.OrderStatusEnum;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 @Builder
 public record OrderSummary(
-        Long orderId,
+        Integer id,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
         LocalDateTime orderDate,
         // Using BigDecimal for monetary values to avoid precision issues.
         BigDecimal totalAmount,
-        OrderStatusEnum status,
-        String paymentMethod
+        String status,
+        String paymentType
 )
 {}
--- src/main/java/spring/practice/elmenus_lite/dto/PaymentResult.java
+++ src/main/java/spring/practice/elmenus_lite/dto/PaymentResult.java
@@ -1,9 +1,9 @@
 package spring.practice.elmenus_lite.dto;
 
-import spring.practice.elmenus_lite.model.enums.PaymentStatusEnum;
+import spring.practice.elmenus_lite.model.enums.TransactionStatusEnum;
 
 public record PaymentResult(
-        PaymentStatusEnum status,    // PENDING, FAILED , SUCCESS
+        TransactionStatusEnum status,    // PENDING, FAILED , SUCCESS
         String paymentMethod,        // Credit Card, Cash, etc.
         String transactionId
 ) {}
