36c994ed146874ada9bc6da33e70686e5a154a87
mahmoud Osama <mahmoudosama@Laptop>
2025-06-02 00:25:34 +0300

    Renamed from PaymentStatusEnum.java to TransactionStatusEnum

--- src/main/java/spring/practice/elmenus_lite/model/enums/TransactionStatusEnum.java
+++ src/main/java/spring/practice/elmenus_lite/model/enums/TransactionStatusEnum.java
@@ -3,19 +3,19 @@ package spring.practice.elmenus_lite.model.enums;
 import lombok.Getter;
 
 @Getter
-public enum PaymentStatusEnum {
+public enum TransactionStatusEnum {
     SUCCESS("SUCCESS"),
     FAILED("FAILED"),
     PENDING("PENDING");
 
     private final String status;
 
-    PaymentStatusEnum(String status) {
+    TransactionStatusEnum(String status) {
         this.status = status;
     }
 
-    public static PaymentStatusEnum fromString(String status) {
-        for (PaymentStatusEnum paymentStatus : PaymentStatusEnum.values()) {
+    public static TransactionStatusEnum fromString(String status) {
+        for (TransactionStatusEnum paymentStatus : TransactionStatusEnum.values()) {
             if (paymentStatus.status.equalsIgnoreCase(status)) {
                 return paymentStatus;
             }
