2f19519374fb2765cfaac3094ea979d7390a7bd5
mahmoud Osama <mahmoudosama@Laptop>
2025-06-02 03:12:15 +0300

    update dtomapper

--- src/main/java/spring/practice/elmenus_lite/mapper/OrderMapper.java
+++ src/main/java/spring/practice/elmenus_lite/mapper/OrderMapper.java
@@ -2,8 +2,14 @@ package spring.practice.elmenus_lite.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import spring.practice.elmenus_lite.dto.DeliveryAddress;
+import spring.practice.elmenus_lite.dto.OrderItemDTO;
 import spring.practice.elmenus_lite.dto.OrderSummary;
+import spring.practice.elmenus_lite.dto.TrackingInfo;
+import spring.practice.elmenus_lite.model.Address;
 import spring.practice.elmenus_lite.model.Order;
+import spring.practice.elmenus_lite.model.OrderItem;
+import spring.practice.elmenus_lite.model.OrderTracking;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
@@ -14,4 +20,11 @@ public interface OrderMapper {
     @Mapping(target = "status", source = "order.orderStatus.name")
     @Mapping(target = "paymentType", source = "paymentType")
     OrderSummary toOrderSummary(Order order, String paymentType);
+
+    DeliveryAddress toDeliveryAddress(Address address);
+
+    @Mapping(target = "name" , source ="menuItem.name")
+    OrderItemDTO toOrderItemDTO(OrderItem orderItem);
+
+    TrackingInfo toTrackingInfo(OrderTracking orderTracking);
 }
