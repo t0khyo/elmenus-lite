15005e6ea6cc03e72c675f854416279804348f3e
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-06-03 11:57:36 +0300

    chore: format CartController

--- src/main/java/spring/practice/elmenus_lite/conroller/CartController.java
+++ src/main/java/spring/practice/elmenus_lite/conroller/CartController.java
@@ -2,34 +2,32 @@ package spring.practice.elmenus_lite.conroller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import spring.practice.elmenus_lite.dto.CartItemRequest;
 import spring.practice.elmenus_lite.dto.CartItemResponse;
 import spring.practice.elmenus_lite.dto.CartResponse;
-import spring.practice.elmenus_lite.model.Customer;
 import spring.practice.elmenus_lite.service.CartService;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/carts")
 @RequiredArgsConstructor
 public class CartController {
     private final CartService cartService;
+
     @PostMapping("/{customerId}/items")
-    public ResponseEntity<CartItemResponse> addItem(@PathVariable("customerId") Integer customerId,@RequestBody @Valid CartItemRequest cartItemRequest) {
-        // todo: Need To Handle Bad Request That Happen because Validation
-         return ResponseEntity.ok(cartService.addItemToCart(customerId,cartItemRequest));
+    public ResponseEntity<CartItemResponse> addItem(
+            @PathVariable Integer customerId,
+            @RequestBody @Valid CartItemRequest cartItemRequest
+    ) {
+        return ResponseEntity.ok(cartService.addItemToCart(customerId, cartItemRequest));
     }
+
     @GetMapping("/{customerId}")
     public ResponseEntity<CartResponse> getCartByCustomerId(@PathVariable Integer customerId) {
-
-        CartResponse cartResponseOpt = cartService.getCartByCustomerId(customerId);
-        return ResponseEntity.ok(cartResponseOpt);
+        return ResponseEntity.ok(cartService.getCartByCustomerId(customerId));
     }
+
     @DeleteMapping("/{cartId}/items/{cartItemId}")
     public ResponseEntity<CartResponse> removeCartItem(
             @PathVariable(name = "cartId") Integer cartId,
@@ -38,9 +36,9 @@ public class CartController {
         return ResponseEntity.ok(cartService.removeCartItem(cartId, cartItemId));
     }
 
-    @DeleteMapping("{cartId}/clear")
+    @DeleteMapping("/{cartId}/clear")
     public ResponseEntity<CartResponse> clearCart(
-            @PathVariable(name = "cartId") Integer cartId
+            @PathVariable Integer cartId
     ) {
         return ResponseEntity.ok(cartService.clearCart(cartId));
     }
