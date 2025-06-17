6984f1e879e7830a081fd3cf6c56375bcfb6cf4f
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-06-17 23:19:33 +0300

    fix: remove cart id from NewOrderRequest

--- src/main/java/spring/practice/elmenus_lite/dto/NewOrderRequest.java
+++ src/main/java/spring/practice/elmenus_lite/dto/NewOrderRequest.java
@@ -5,13 +5,16 @@ import jakarta.validation.constraints.NotNull;
 public record NewOrderRequest(
         @NotNull(message = "Customer Id is required")
         Integer customerId,
+
         @NotNull(message = "Cart Id is required")
-        Integer cartId,
-        @NotNull(message = "Restaurant Id is required")
         Integer restaurantId,
+
         @NotNull(message = "Address Id is required")
         Integer addressId, // Must reference existing address
+
         @NotNull(message = "preferredPaymentSetting Id is required")
         Integer preferredPaymentSettingId,  // can only use their saved payment methods
+
         String promotionCode
-) {}
+) {
+}
