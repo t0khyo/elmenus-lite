16584624e17b740d28bcd2b8a848c06458cc5fb6
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-06-24 12:13:02 +0300

    fix: OrderRequest dto validation message

--- src/main/java/spring/practice/elmenus_lite/dto/OrderRequest.java
+++ src/main/java/spring/practice/elmenus_lite/dto/OrderRequest.java
@@ -6,13 +6,13 @@ public record OrderRequest(
         @NotNull(message = "Customer Id is required")
         Integer customerId,
 
-        @NotNull(message = "Cart Id is required")
+        @NotNull(message = "Restaurant Id is required")
         Integer restaurantId,
 
         @NotNull(message = "Address Id is required")
         Integer addressId, // Must reference existing address
 
-        @NotNull(message = "preferredPaymentSetting Id is required")
+        @NotNull(message = "Preferred Payment Setting Id is required")
         Integer preferredPaymentSettingId,  // can only use their saved payment methods
 
         String promotionCode
