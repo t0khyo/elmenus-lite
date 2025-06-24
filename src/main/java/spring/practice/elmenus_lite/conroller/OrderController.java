f8765a8c5e6dea974b7e34c643ced5bf60137d8e
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-06-24 19:28:58 +0300

    fix: remove unnecessary comments and methods at OrderController & OrderServiceImpl

--- src/main/java/spring/practice/elmenus_lite/conroller/OrderController.java
+++ src/main/java/spring/practice/elmenus_lite/conroller/OrderController.java
@@ -13,44 +13,10 @@ import spring.practice.elmenus_lite.service.OrderService;
 public class OrderController {
     private final OrderService orderService;
 
-    // POST api/v1/orders - Place new order
-    @PostMapping
-    public ResponseEntity<OrderSummary> placeOrder(@RequestBody NewOrderRequest newOrderRequest) {
-        OrderSummary summary = orderService.placeOrder(newOrderRequest);
-        return ResponseEntity.status(201).body(summary);
-    }
-
-//    // PUT /orders/{orderId}/cancel - Cancel order
-//    @PutMapping("/{orderId}/cancel")
-//    public ResponseEntity<OrderSummary> cancelOrder(
-//            @PathVariable Long orderId,
-//            @RequestBody CancelOrderRequest request
-//    ) {
-//        OrderSummary summary = orderService.cancelOrder(orderId, request);
-//        return ResponseEntity.ok(summary);
-//    }
-//
-//    // PUT /orders/{orderId}/status - Update order status
-//    @PutMapping("/{orderId}/status")
-//    public ResponseEntity<OrderSummary> updateOrderStatus(
-//            @PathVariable Long orderId,
-//            @RequestBody UpdateOrderStatusRequest request
-//    ) {
-//        OrderSummary summary = orderService.updateOrderStatus(orderId, request);
-//        return ResponseEntity.ok(summary);
-//    }
-//
     // GET /orders/{orderId}/summary - Get order summary
     @GetMapping("/{orderId}/summary")
     public ResponseEntity<OrderSummary> getOrderSummary(@PathVariable Integer orderId) {
         OrderSummary summary = orderService.getOrderSummary(orderId);
         return ResponseEntity.ok(summary);
     }
-
-//    // GET /orders/{orderId}/details - Get full order details
-//    @GetMapping("/{orderId}/details")
-//    public ResponseEntity<OrderDetails> getOrderDetails(@PathVariable Long orderId) {
-//        OrderDetails details = orderService.getOrderDetails(orderId);
-//        return ResponseEntity.ok(details);
-//    }
 }
