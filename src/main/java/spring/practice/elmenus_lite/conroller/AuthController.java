f1bbbbfb2b1b887ff43908ec6783f8f422e763c5
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-07-16 18:46:24 +0300

    feat: implement AuthController

---
+++ src/main/java/spring/practice/elmenus_lite/conroller/AuthController.java
@@ -0,0 +1,31 @@
+package spring.practice.elmenus_lite.conroller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.practice.elmenus_lite.dto.request.LoginRequest;
+import spring.practice.elmenus_lite.dto.request.SignupRequest;
+import spring.practice.elmenus_lite.dto.response.LoginResponse;
+import spring.practice.elmenus_lite.service.AuthService;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@Valid SignupRequest signupRequest) {
+        authService.signup(signupRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+}
