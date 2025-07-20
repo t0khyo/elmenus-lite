d66d90a6781d4ca4d82228adbc055c6477bbe9c4
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-07-20 21:03:57 +0300

    fix: add @Repository

--- src/main/java/spring/practice/elmenus_lite/repostory/PreferredPaymentSettingRepository.java
+++ src/main/java/spring/practice/elmenus_lite/repostory/PreferredPaymentSettingRepository.java
@@ -1,7 +1,9 @@
 package spring.practice.elmenus_lite.repostory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import spring.practice.elmenus_lite.model.PreferredPaymentSetting;
 
+@Repository
 public interface PreferredPaymentSettingRepository extends JpaRepository<PreferredPaymentSetting, Integer> {
 }
