b150be35461b07c4d0a8f5337b173671bca46bc6
Abdelrahman Eltokhy <abdelrahman.m.eltokhy@gmail.com>
2025-07-21 15:58:50 +0300

    feat: add AuditorAwareConfig

--- src/main/java/spring/practice/elmenus_lite/config/AuditorAwareConfig.java
+++ src/main/java/spring/practice/elmenus_lite/config/AuditorAwareConfig.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import spring.practice.elmenus_lite.model.User;
 
 import java.util.Optional;
 
