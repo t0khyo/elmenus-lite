5ddc1d9e450903b2e58886544b10f5f96326d867
mahmoud Osama <mahmoudosama@Laptop>
2025-08-05 03:59:26 +0300

    feat(repository): add findByUserId method to CustomerRepository

--- src/main/java/spring/practice/elmenus_lite/repostory/CustomerRepository.java
+++ src/main/java/spring/practice/elmenus_lite/repostory/CustomerRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import spring.practice.elmenus_lite.model.Customer;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByUserId(Integer userId);
 
 }
