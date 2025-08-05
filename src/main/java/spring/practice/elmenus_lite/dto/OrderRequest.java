e0cfdc5a878955c3ac6448e7ac0aa077ab864529
mahmoud Osama <mahmoudosama@Laptop>
2025-08-05 04:00:06 +0300

    feat(dto): update OrderRequest

--- src/main/java/spring/practice/elmenus_lite/dto/OrderRequest.java
+++ src/main/java/spring/practice/elmenus_lite/dto/OrderRequest.java
@@ -3,12 +3,6 @@ package spring.practice.elmenus_lite.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record OrderRequest(
-        @NotNull(message = "Customer Id is required")
-        Integer customerId,
-
-        @NotNull(message = "Restaurant Id is required")
-        Integer restaurantId,
-
         @NotNull(message = "Address Id is required")
         Integer addressId, // Must reference existing address
 
